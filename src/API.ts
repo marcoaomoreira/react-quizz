29a4f6141926be4757363a5a7868117c6b0a335f
Markim <40443821+marcoaomoreira@users.noreply.github.com>
2020-07-26 23:39:36 -0300

    Update API.ts

--- src/API.ts
+++ src/API.ts
@@ -19,7 +19,7 @@ export enum Difficulty{
     HARD = "hard",
 };
 export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty ) => {
-    const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+    const endpoint = `https://opentdb.com/api.php?amount=${amount}&category=14&difficulty=${difficulty}&type=multiple`;
     const data = await (await fetch(endpoint)).json();
     return data.results.map((question: Question) => ({
             ...question,
